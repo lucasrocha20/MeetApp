6edef37206cc7352c20ce72661c7c61f76b4361a
Lucas Rocha <lucas_rocha.14@outlook.com>
2019-09-30 21:41:32 -0300

    chore: env add

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,8 +1,8 @@
-import jwt from 'jsonwebtoken';
-import * as Yup from 'yup';
+import jwt from "jsonwebtoken";
+import * as Yup from "yup";
 
-import User from '../models/User';
-import authConfig from '../../config/auth';
+import User from "../models/User";
+import authConfig from "../../config/auth";
 
 class SessionController {
   async store(req, res) {
@@ -10,22 +10,22 @@ class SessionController {
       email: Yup.string()
         .email()
         .required(),
-      password: Yup.string().required(),
+      password: Yup.string().required()
     });
 
     if (!schema.isValid(req.body)) {
-      return res.status(400).json('Validation is fails');
+      return res.status(400).json("Validation is fails");
     }
 
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(401).json({ error: 'User does not exist' });
+      return res.status(401).json({ error: "User does not exist" });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Wrong password' });
+      return res.status(401).json({ error: "Wrong password" });
     }
 
     const { id, name } = user;
@@ -33,8 +33,8 @@ class SessionController {
     return res.json({
       user: { id, name },
       token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
+        expiresIn: authConfig.expiresIn
+      })
     });
   }
 }
